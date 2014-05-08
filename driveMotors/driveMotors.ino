54f687ec56578319eef941e259ce392596380752
Kaitlin Gallagher <kkgallagher9@gmail.com>
2014-05-08 17:37:15 -0400

    motors
    getting started

---
+++ driveMotors/driveMotors.ino
@@ -0,0 +1,27 @@
+/*
+Code taken from adafruit tutorial:
+https://learn.adafruit.com/adafruit-arduino-lesson-13-dc-motors/arduino-code
+*/
+
+int motorPin = 3;
+ 
+void setup() 
+{ 
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+} 
+ 
+ 
+void loop() 
+{ 
+  if (Serial.available())
+  {
+    int speed = Serial.parseInt();
+    if (speed >= 0 && speed <= 255)
+    {
+      analogWrite(motorPin, speed);
+    }
+  }
+} 
